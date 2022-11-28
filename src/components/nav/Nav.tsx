5ee11d5848c601cddd5560edd6c6fe4b4e7475be
moise <mose.ns@hotmail.com>
2022-11-28 16:46:11 +0100

    categories component + css nav

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,40 +1,19 @@
 import "./nav.scss";
-import { useState } from "react";
-import { Listbox } from "@headlessui/react";
-import { IoMdArrowDropdown } from "react-icons/io";
 import { AiOutlineHeart } from "react-icons/ai";
 import { MdOutlineShoppingBag } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
-import { womenLinks } from "../../utils/linksNav";
+import Listcategories from "./Listcategories";
 
 type Props = {};
 
-const categories = [
-  { id: 1, category: "Women" },
-  { id: 2, category: "Men" },
-];
 
 const Nav = (props: Props) => {
-  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
 
   return (
     <nav className="navbar-container">
       <div className="logo">needle</div>
       <div className="nav-content">
-        <Listbox value={selectedCategory} onChange={setSelectedCategory}>
-          <Listbox.Button className="category-container">
-            <span className="selected-category">
-              {selectedCategory.category} {<IoMdArrowDropdown />}
-            </span>
-          </Listbox.Button>
-          <Listbox.Options className="listcategory-options">
-            {categories.map((category) => (
-              <Listbox.Option key={category.id} value={category} className="category-option">
-                {category.category}
-              </Listbox.Option>
-            ))}
-          </Listbox.Options>
-        </Listbox>
+        <Listcategories />
         <ul className="uls-list">
           <li>Home</li>
           <li>Clothes</li>
