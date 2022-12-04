b04184cc261de00992f65e0bd45dc9bd0783a35d
moise <mose.ns@hotmail.com>
2022-12-04 23:10:41 +0100

    quotes part 2

---
+++ src/utils/quotes.ts
@@ -0,0 +1,35 @@
+export const quoteOne = [
+  {
+    id: "9064a7f6-7373",
+    quote: "Being one step ahead of the fashion trend is not so important to me. What matters is always moving forward - Sonia Rykiel",
+    image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1645771930/e-commerce-daddy-will-pay/tmp-4-1645771929434_bjllop.png",
+    color: "red"
+
+  }
+]
+export const quoteTwo = [
+  {
+    id: "9064aa4e-7373",
+    quote: "Fashion passes, style remains» - Yves Saint-Laurent",
+    image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1645771930/e-commerce-daddy-will-pay/tmp-4-1645771929434_bjllop.png",
+    color: "transparent"
+  }
+]
+export const quoteThree = [
+  {
+    id: "9064ab48-7373",
+    quote: "Being one step ahead of the fashion trend is not so important to me. What matters is always moving forward - Sonia Rykiel",
+    image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1645771930/e-commerce-daddy-will-pay/tmp-4-1645771929434_bjllop.png",
+    color: "lightpurple"
+
+  }
+]
+export const quoteFour = [
+  {
+    id: "9064ac2e-7373",
+    quote: "Being one step ahead of the fashion trend is not so important to me. What matters is always moving forward - Sonia Rykiel",
+    image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1645771930/e-commerce-daddy-will-pay/tmp-4-1645771929434_bjllop.png",
+    color: "lightyellow"
+
+  }
+]
