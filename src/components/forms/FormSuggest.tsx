c85631a7d74197e76e50691f7e0b14ef84c051f0
moise <mose.ns@hotmail.com>
2022-12-05 14:56:56 +0100

    form suggest component

--- src/components/forms/FormSuggest.tsx
+++ src/components/forms/FormSuggest.tsx
@@ -1,14 +1,38 @@
-import "../suggestions/suggest.scss"
+import "../suggestions/suggest.scss";
 
-
-
-
-type Props = {}
+type Props = {};
 
 const FormSuggest = (props: Props) => {
   return (
-    <form className="form-suggest-contenair">FormSuggest</form>
-  )
-}
+    <form className="form-suggest-contenair">
+      <div className="suggest-input-names">
+        <label htmlFor="firstname" />
+        <input
+          type="text"
+          className="suggest-names"
+          placeholder="First name"
+          aria-label="Input firstname"
+        />
+        <label htmlFor="lastname" />
+        <input
+          type="text"
+          className="suggest-names"
+          placeholder="Last name"
+          aria-label="Input lastname"
+        />
+      </div>
+      <label htmlFor="email" />
+      <input
+        type="email"
+        name="email"
+        className="suggest-email"
+        aria-label="Input email"
+      />
+      <label htmlFor="text-area" />
+      <textarea name="" id="" cols={30} rows={10} aria-label="textarea field" />
+      <button type="submit">SEND</button>
+    </form>
+  );
+};
 
 export default FormSuggest;
