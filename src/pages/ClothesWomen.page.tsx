4a981d68d7e8d18f867a8bd37e6d11265dc68b42
moise <mose.ns@hotmail.com>
2022-12-11 04:35:12 +0100

    Stories page component

--- src/pages/ClothesWomen.page.tsx
+++ src/pages/ClothesWomen.page.tsx
@@ -1,6 +1,7 @@
 import "../pages/pages.scss";
 import Headerclothes from "../components/headers/Headerclothes";
 import Clothescontainer from "../components/clothesContainer/Clothescontainer";
+import Stories from "../components/story/Stories";
 import Quotes from "../components/quotesComponent/Quotes";
 import { quoteThree } from "../utils/quotes";
 
@@ -11,6 +12,7 @@ const ClothesWomen = () => {
       <Headerclothes />
       <Clothescontainer />
       <Quotes quotes={quoteThree} />
+      <Stories />
     </main>
   )
 }
