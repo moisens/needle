c50c009faf5f90d9de26bdf34ba40f56bf90b977
moise <mose.ns@hotmail.com>
2022-12-12 01:02:25 +0100

    quotes modif

--- src/components/quotesComponent/Quotes.tsx
+++ src/components/quotesComponent/Quotes.tsx
@@ -4,15 +4,15 @@ import { QuotesProps } from "../../types/typesQuotes";
 
 const getDifferentBackgroung = (color: string) => {
   if (color === "red") {
-    return <section className="quotes-content-img"></section>
+    return <section className="quotes-content-img"></section>;
   } else if (color === "transparent") {
-    return <section className="quotes-content-img2"></section>
+    return <section className="quotes-content-img2"></section>;
   } else if (color === "lightpurple") {
-    return <section className="quotes-content-img3"></section>
+    return <section className="quotes-content-img3"></section>;
   } else {
-    return <section className="quotes-content-img4"></section>
+    return <section className="quotes-content-img4"></section>;
   }
-}
+};
 
 const Quotes = ({ quotes }: QuotesProps) => {
   return (
@@ -21,13 +21,18 @@ const Quotes = ({ quotes }: QuotesProps) => {
         const { id, quote, image, color } = quot;
         return (
           <section className="quotes-content" key={id}>
-            <section className="quotes-big-image">
-              <Images src={image} alt="lady" title="lady" />
+            <section
+              className={`${
+                color === "transparent" || color === "lightyellow"
+                  ? "quotes-big-image big-inner-image"
+                  : "quotes-big-image"
+              }`}
+            >
+              <Images src={image} alt="product" title="product" />
             </section>
             <q className="quotes-quote">{quote}</q>
             {getDifferentBackgroung(color)}
           </section>
-          
         );
       })}
     </section>
--- src/utils/quotes.ts
+++ src/utils/quotes.ts
@@ -11,7 +11,7 @@ export const quoteTwo = [
   {
     id: "9064aa4e-7373",
     quote: "Fashion passes, style remains» - Yves Saint-Laurent",
-    image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1645771930/e-commerce-daddy-will-pay/tmp-4-1645771929434_bjllop.png",
+    image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1670797242/needle-1/talons-1_y8a4nu.png",
     color: "transparent"
   }
 ]
@@ -28,7 +28,7 @@ export const quoteFour = [
   {
     id: "9064ac2e-7373",
     quote: "Being one step ahead of the fashion trend is not so important to me. What matters is always moving forward - Sonia Rykiel",
-    image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1645771930/e-commerce-daddy-will-pay/tmp-4-1645771929434_bjllop.png",
+    image: "https://res.cloudinary.com/dr7qigh2d/image/upload/v1670797252/needle-1/talon-2_p27xca.png",
     color: "lightyellow"
 
   }
