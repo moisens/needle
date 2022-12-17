70706f636ffcf13acd70a05942a7a5b94f916089
moise <mose.ns@hotmail.com>
2022-12-18 00:20:25 +0100

    form suggest functionality

--- src/components/forms/FormSuggest.tsx
+++ src/components/forms/FormSuggest.tsx
@@ -1,24 +1,61 @@
 import "../suggestions/suggest.scss";
+import { useState } from "react";
 
-type Props = {};
+interface FormProps {
+  firstname: string;
+  lastname: string;
+  email: string;
+}
 
-const FormSuggest = (props: Props) => {
+
+
+const FormSuggest = () => {
+  const [form, setForm] = useState<FormProps>({
+    firstname: "",
+    lastname: "",
+    email: "",
+  });
+  const [textArea, setTextArea] = useState("")
+
+  const handleSugestionSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(form, textArea);
+  };
+
+  const handleForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextArea(e.target.value);
+    
+  }
+
   return (
-    <form className="form-suggest-contenair">
+    <form className="form-suggest-contenair" onSubmit={handleSugestionSubmit}>
       <div className="suggest-input-names">
         <label htmlFor="firstname" />
         <input
           type="text"
+          name="firstname"
           className="suggest-names"
           placeholder="First name"
           aria-label="Input firstname"
+          value={form.firstname}
+          onChange={handleForm}
         />
         <label htmlFor="lastname" />
         <input
           type="text"
+          name="lastname"
           className="suggest-names"
           placeholder="Last name"
           aria-label="Input lastname"
+          value={form.lastname}
+          onChange={handleForm}
         />
       </div>
       <label htmlFor="email" />
@@ -28,14 +65,18 @@ const FormSuggest = (props: Props) => {
         className="suggest-email"
         aria-label="Input email"
         placeholder="Email"
+        value={form.email}
+        onChange={handleForm}
       />
       <label htmlFor="text-area" />
       <textarea
-        name="text-area"
+        name="textarea"
         cols={3}
         rows={5}
         placeholder="Subject"
         aria-label="textarea field"
+        value={textArea}
+        onChange={handleTextArea}
       />
       <button
         type="submit"
