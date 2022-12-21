f48a81ef04254e2c8cc747d370090c0dccb5ad86
moise <mose.ns@hotmail.com>
2022-12-21 17:03:37 +0100

    dropdown functionality + css

--- src/components/nav/Navlist.tsx
+++ src/components/nav/Navlist.tsx
@@ -1,24 +1,59 @@
 import { NavLink } from "react-router-dom";
-import { LinkProps } from "../../types/typeLinks";
+import { Menu } from "@headlessui/react";
+import { navWomenClothes } from "../../utils/linksNav";
+import { RiArrowDownSFill } from "react-icons/ri"
 
-const Navlist = ({ links }: LinkProps) => {
+const Navlist = () => {
   return (
     <ul className="uls-list">
-      {links.map((link) => {
-        const { id, url, text } = link;
-        return (
-          <li key={id}>
+      <Menu>
+        <Menu.Button className="btn-btn-menu">
+          <li>
             <NavLink
-              to={url}
+              to="women/women-clothes"
               className={({ isActive }) =>
                 isActive ? "li-a activeLink" : "li-a"
               }
             >
-              {text}
+              Clothes
             </NavLink>
           </li>
-        );
-      })}
+          <RiArrowDownSFill />
+        </Menu.Button>
+        <Menu.Items className="menu-items">
+          <section className="menu-item">
+          {navWomenClothes.map((link) => (
+            <Menu.Item key={link.id}>
+              <NavLink to={`women/women-clothes/${link.url}`} className={({ isActive }) => isActive ? 'nav-link-item active-item' : 'nav-link-item'}>
+                {link.text}
+              </NavLink>
+            </Menu.Item>
+          ))}
+          </section>
+          <section>Image</section>
+        </Menu.Items>
+      </Menu>
+      <Menu>
+      <Menu.Button className="btn-btn-menu">
+          <li>
+            <NavLink
+              to="women/women-shoes"
+              className={({ isActive }) =>
+                isActive ? "li-a activeLink" : "li-a"
+              }
+            >
+              Shoes
+            </NavLink>
+          </li>
+        </Menu.Button>
+        <Menu.Items>
+          {navWomenClothes.map((link) => (
+            <Menu.Item key={link.id}>
+              <NavLink to={link.url}>{link.text}</NavLink>
+            </Menu.Item>
+          ))}
+        </Menu.Items>
+      </Menu>
     </ul>
   );
 };
