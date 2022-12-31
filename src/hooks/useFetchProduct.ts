6281835d5c46362c52616b210ec08ec9b5e9ef49
moise <mose.ns@hotmail.com>
2022-12-31 23:55:41 +0100

    fecth functions

--- src/hooks/useFetchProduct.ts
+++ src/hooks/useFetchProduct.ts
@@ -6,6 +6,32 @@ const fetchProduct = async (): Promise<ResponseIProducts> => {
   return await axios.get("/api/v1/products");
 };
 
+const fetchClothes = async (): Promise<ResponseIProducts> => {
+  return await axios.get(`/api/v1/products?maincategories="women"&subcategories="all"`);
+}
+
+const fetchJackets = async (): Promise<ResponseIProducts> => {
+  return await axios.get(`/api/v1/products?maincategories="women"&subcategories="jacket"`)
+} 
+const fetchShirts = async (): Promise<ResponseIProducts> => {
+  return await axios.get(`/api/v1/products?maincategories="women"&subcategories="shirt"`)
+} 
+const fetchPants = async (): Promise<ResponseIProducts> => {
+  return await axios.get(`/api/v1/products?maincategories="women"&subcategories="pants"`)
+} 
+const fetchSuits = async (): Promise<ResponseIProducts> => {
+  return await axios.get(`/api/v1/products?maincategories="women"&subcategories="suit"`)
+} 
+
+
+export const useFetchClothes = () => {
+  return useQuery<ResponseIProducts, Error>(["clothes"], fetchClothes)
+}
+
+export const useFetchJackets = () => {
+  return useQuery<ResponseIProducts, Error>(["jacket"], fetchJackets);
+}
+
 const useFetchproducts = () => {
   return useQuery<ResponseIProducts, Error>(["products"], fetchProduct);
 };
