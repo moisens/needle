e241a29b9f029a847a0445e25188165ba851e2d6
moise <mose.ns@hotmail.com>
2023-01-27 16:10:00 +0100

    single product imgs products

--- frontend/src/components/singleProduct/Singleproduct.tsx
+++ frontend/src/components/singleProduct/Singleproduct.tsx
@@ -1,5 +1,6 @@
 import "./singleproduct.scss";
 import { TiArrowLeftThick } from "react-icons/ti";
+import Images from "../imageComponent/Images";
 
 
 
@@ -9,7 +10,30 @@ const Singleproduct = () => {
       <section className="singlepage-content">
         <header className="singlepage-header">
           <section className="singlepage-backhome"><TiArrowLeftThick size="1.8rem" />Previous page</section>
-          <section className="singlepage-product"></section>
+          <section className="singlepage-product">
+            <section className="image-container">
+              <section className="big-image">
+                <Images src="https://res.cloudinary.com/dr7qigh2d/image/upload/v1646882533/e-commerce-daddy-will-pay/tmp-5-1646882532306_w63tjk.png" alt="tiny img prod" title="title prod" />
+              </section>
+              <section className="tiny-images-container">
+                <section className="tiny-image">
+                  <Images src="https://res.cloudinary.com/dr7qigh2d/image/upload/v1646882533/e-commerce-daddy-will-pay/tmp-5-1646882532306_w63tjk.png" alt="tiny img prod" title="title prod" />
+                </section>
+                <section className="tiny-image">
+                  <Images src="https://res.cloudinary.com/dr7qigh2d/image/upload/v1646882533/e-commerce-daddy-will-pay/tmp-5-1646882532306_w63tjk.png" alt="tiny img prod" title="title prod" />
+                </section>
+                <section className="tiny-image">
+                  <Images src="https://res.cloudinary.com/dr7qigh2d/image/upload/v1646882533/e-commerce-daddy-will-pay/tmp-5-1646882532306_w63tjk.png" alt="tiny img prod" title="title prod" />
+                </section>
+                <section className="tiny-image">
+                  <Images src="https://res.cloudinary.com/dr7qigh2d/image/upload/v1646882533/e-commerce-daddy-will-pay/tmp-5-1646882532306_w63tjk.png" alt="tiny img prod" title="title prod" />
+                </section>
+              </section>
+            </section>
+            <section className="single-product-infos">
+              product
+            </section>
+          </section>
         </header>
       </section>
     </section>
