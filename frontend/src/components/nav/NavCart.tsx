eb10177d64fa1e85cb66341f5b96c6b5960a3c60
moise <mose.ns@hotmail.com>
2023-05-18 22:42:48 +0200

    cart nav component css

--- frontend/src/components/nav/NavCart.tsx
+++ frontend/src/components/nav/NavCart.tsx
@@ -1,18 +1,48 @@
+import Images from "../imageComponent/Images";
 import { MdOutlineShoppingBag } from "react-icons/md";
 import useCart from "../../hooks/useCart";
+import Button from "../button/Button";
 
 const NavCart = () => {
-  const { totalItems } = useCart();
-
-  const handleHoverCart = () => {
-    console.log("hovering...");
-    
-  }
+  const { totalItems, cart } = useCart();
 
   return (
-    <section className="carts-container" onMouseEnter={handleHoverCart}>
-      <MdOutlineShoppingBag size="1.875rem" />
+    <section className="carts-container">
+      <MdOutlineShoppingBag size="1.8rem" className="cart-cart-shopping-icon" />
       <div className="cart-count">{totalItems}</div>
+      <div className="cart-content">
+        <h4 className="cart-heading-4">My Shopping Cart</h4>
+        <section className="cart-articles-container">
+          <div className="cart-cart-articles">
+            <article className="cart-article-content">
+              <div className="cart-image-content">
+                <Images src="" alt="" title="" />
+              </div>
+              <div className="cart-infos-container">
+                <div>
+                  <p className="cart-p">Van Brughel</p>
+                  <p className="cart-p">Size: Medium</p>
+                  <p className="cart-p">Quantity: 1</p>
+                </div>
+                <div className="cart-price">
+                  <p className="cart-p">€1800</p>
+                </div>
+                <div className="cart-functionality">
+                  <div>
+                    <p className="cart-p">Move to Favorite</p>
+                  </div>
+                  <div>
+                    <p className="cart-p">Delete</p>
+                  </div>
+                </div>
+              </div>
+            </article>
+          </div>
+          <Button as="button" className="cart-btn-btn">
+            Go To Cart
+          </Button>
+        </section>
+      </div>
     </section>
   );
 };
