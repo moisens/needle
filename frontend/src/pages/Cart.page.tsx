e904ebb467758788caddaa2ad9fbb21a559daae5
moise <mose.ns@hotmail.com>
2023-05-22 18:34:45 +0200

    cart page css part 1

--- frontend/src/pages/Cart.page.tsx
+++ frontend/src/pages/Cart.page.tsx
@@ -1,10 +1,40 @@
+import "../components/cart/cart.scss";
+import Images from "../components/imageComponent/Images";
 
 
-type Props = {}
 
-const Cartpage = (props: Props) => {
+const Cartpage = () => {
   return (
-    <div>Cartpage</div>
+    <main className="pages-container">
+      <section className="cartpage-container">
+        <section className="cart-items-numbers">
+          <h3>My Cart (3 items)</h3>
+        </section>
+        <header className="cart-header">
+          <section className="cartpage-articles-container">
+            <article className="articles-content">
+              <div className="article-image-container">
+                <Images src="" alt="Cart" title="cart" />
+              </div>
+              <div className="article-infos-container">
+                <div className="article-infos-infos">
+                  <div className="article-infos-content">
+                    <h3>Tailor name</h3>
+                    <p>color: red</p>
+                    <p>size: L</p>
+                  </div>
+                  <div className="article-infos-content">
+                    
+                  </div>
+                </div>
+                <div className="article-infos-price">delete Move to Price</div>
+              </div>
+            </article>
+          </section>
+          <section className="cartpage-total-container">2</section>
+        </header>
+      </section>
+    </main>
   )
 }
 
