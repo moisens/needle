9ffa7f58be9aeab6ab16ea956230647d7bf48ce9
moise <mose.ns@hotmail.com>
2023-05-26 13:29:32 +0200

    cart empty cart functionality

--- frontend/src/pages/Cart.page.tsx
+++ frontend/src/pages/Cart.page.tsx
@@ -9,41 +9,74 @@ const Cartpage = () => {
   return (
     <main className="pages-container">
       <section className="cartpage-container">
-        <section className="cart-items-numbers">
+        <section
+          className={`${
+            totalItems <= 0
+              ? "cart-items-numbers hidden-number"
+              : "cart-items-numbers"
+          }`}
+        >
           <h3>{`My Cart (${totalItems} ${
             totalItems > 1 ? "items" : "item"
           })`}</h3>
         </section>
-        <header className="cart-header">
-          <section className="cartpage-articles-container">
-            {cart.map((item) => (
-              <CartArticleComponent key={item._id} {...item} />
-            ))}
-          </section>
-          <section className="cartpage-total-container">
-            <div className="checkout-product-content">
-              <h3 className="checkout-h3">Total</h3>
-              <div>
-                <p>Subtotal</p>
-                <p className="price-checkout">{totalPrice}</p>
+        {totalItems > 0 ? (
+          <header className="cart-header">
+            <section className="cartpage-articles-container">
+              {cart.map((item) => (
+                <CartArticleComponent key={item._id} {...item} />
+              ))}
+            </section>
+            <section className="cartpage-total-container">
+              <div className="checkout-product-content">
+                <h3 className="checkout-h3">Total</h3>
+                <div>
+                  <p>Subtotal</p>
+                  <p className="price-checkout">{totalPrice}</p>
+                </div>
+                <div>
+                  <p>Shipping</p>
+                  <p className="price-checkout">0,00 €</p>
+                </div>
+                <div>
+                  <p>
+                    <span className="total-bold">Total</span>{" "}
+                    <span className="checkoout-tax">(including vta)</span>
+                  </p>
+                  <p className="price-checkout">{totalPrice}</p>
+                </div>
+                <Button as="button" className="checkout-btn">
+                  Checkout
+                </Button>
               </div>
-              <div>
-                <p>Shipping</p>
-                <p className="price-checkout">0,00 €</p>
+            </section>
+          </header>
+        ) : (
+          <section className="cart-empty">
+            <div className="cart-emty-template">
+              <div className="fake-img-square"></div>
+              <div className="fake-text-section">
+                <div className="fake-text-line"></div>
+                <div className="fake-text-line short"></div>
+                <div className="fake-img-container">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <div className="fake-btn">+</div>
               </div>
-              <div>
-                <p>
-                  <span className="total-bold">Total</span>{" "}
-                  <span className="checkoout-tax">(including vta)</span>
-                </p>
-                <p className="price-checkout">{totalPrice}</p>
-              </div>
-              <Button as="button" className="checkout-btn">
-                Checkout
+            </div>
+            <div className="cart-emty-infos">
+              <h4 className="cart-emty-h4">Your cart is empty!</h4>
+              <p>Would you like to add some products?</p>
+              {/* //TODO: conditionally change the text: user ? 'something' : 'something else' */}
+              <Button as="button" className="empty-btn-btn">
+                Connect
               </Button>
+              {/* //TODO: conditionally change the Button text: user ? 'Go to Homepage' : 'something else' */}
             </div>
           </section>
-        </header>
+        )}
       </section>
     </main>
   );
