a66469e78015ed8c95f50622189c4bcce8c8d993
moise <mose.ns@hotmail.com>
2023-06-01 14:02:43 +0200

    single product modifications, using detai component

--- frontend/src/components/singleProduct/Singleproduct.tsx
+++ frontend/src/components/singleProduct/Singleproduct.tsx
@@ -1,16 +1,13 @@
 import "./singleproduct.scss";
-import { TiArrowLeftThick } from "react-icons/ti";
 import Images from "../imageComponent/Images";
-import { Disclosure } from "@headlessui/react";
 import { RiArrowDownSFill } from "react-icons/ri";
-import { FaStar } from "react-icons/fa";
-import { FaStarHalf } from "react-icons/fa";
 import { BsArrowLeft } from "react-icons/bs";
-//import { BsHeartFill } from "react-icons/bs"
 import { CiHeart } from "react-icons/ci";
 import { useFetchSingleProduct } from "../../hooks/useFetchProduct";
 import { useParams } from "react-router-dom";
 import DetailComponent from "./DetailComponent";
+import { CgRadioCheck } from "react-icons/cg";
+import Button from "../button/Button";
 
 const Singleproduct = () => {
   const { _id } = useParams();
@@ -23,10 +20,71 @@ const Singleproduct = () => {
   return (
     <section className="singlepage-container">
       <header className="singlepage-header">
-        <div className="detail-back">
-          <BsArrowLeft size="1.8rem" />
-        </div>
-        <DetailComponent />
+        <DetailComponent>
+          <div className="single-back">
+            <BsArrowLeft size="1.8rem" /> <p>Back to clothes</p>
+          </div>
+
+          <section className="single-container">
+            <div className="single-single-content">
+              <section className="single-images-container">
+                <div className="single-slide">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <div className="single-img">
+                  <Images src="" alt="" title="" />
+                </div>
+              </section>
+              <article className="single-article">
+                <h3 className="single-heading-3">Tailor name</h3>
+                <div className="single-pricing">
+                  <h4 className="single-heading-4">1600€</h4>
+                  <p>Includes VTA</p>
+                </div>
+                <p className="single-p">
+                  <span className="single-color">Color</span>: red
+                </p>
+                <div className="single-slide-article">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+                <div className="single-size-container">
+                  <div className="single-size-title">
+                    <p>Select your size</p>
+                    <RiArrowDownSFill />
+                  </div>
+                  <div className="single-size-content">
+                    <div className="single-size-size">
+                      <div className="single-size">
+                        <CgRadioCheck size="1.8rem" />
+                        <p>L</p>
+                      </div>
+                      <p>Available</p>
+                    </div>
+                    <div className="single-size-size">
+                      <div className="single-size">
+                        <CgRadioCheck size="1.8rem" />
+                        <p>M</p>
+                      </div>
+                      <p>Available</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="single-add-like-btns">
+                  <Button as="button" className="single-add-btn">
+                    Add to Cart
+                  </Button>
+                  <CiHeart size="2.5rem" />
+                </div>
+              </article>
+            </div>
+          </section>
+        </DetailComponent>
       </header>
     </section>
   );
