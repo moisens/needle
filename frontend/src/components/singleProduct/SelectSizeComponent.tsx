0a75358d234b7c62db525370f21fdb4c0d2d3637
moise <mose.ns@hotmail.com>
2023-06-02 13:53:35 +0200

    selecting size functionality

--- frontend/src/components/singleProduct/SelectSizeComponent.tsx
+++ frontend/src/components/singleProduct/SelectSizeComponent.tsx
@@ -1,14 +1,27 @@
 import { CgRadioCheck } from "react-icons/cg";
+import { MdRadioButtonChecked } from "react-icons/md";
 
 export type SizeProp = {
   onesize: string;
+  chosenSize: string;
+  setChoseSize: React.Dispatch<React.SetStateAction<string>>;
+  setIsOn: React.Dispatch<React.SetStateAction<boolean>>
 };
 
-const SelectSize = ({ onesize }: SizeProp) => {
+const SelectSize = ({ onesize, chosenSize, setChoseSize, setIsOn }: SizeProp) => {
+  const handleChosenSize = () => {
+    setChoseSize(onesize);
+    setIsOn(false)
+  };
   return (
-    <div className="single-size-size">
+    <div className="single-size-size" onClick={handleChosenSize}>
       <div className="single-size">
-        <CgRadioCheck size="1.8rem" />
+        {chosenSize !== onesize ? (
+          <CgRadioCheck size="1.8rem" />
+        ) : (
+          <MdRadioButtonChecked size="1.8rem" />
+        )}
+
         <p>{onesize}</p>
       </div>
       <p>Available</p>
