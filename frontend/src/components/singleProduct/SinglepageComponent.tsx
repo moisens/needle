aff204970e71e413c020f019826cfee9c66cc3c4
moise <mose.ns@hotmail.com>
2023-06-06 10:55:41 +0200

    single product custom css added + created new component

---
+++ frontend/src/components/singleProduct/SinglepageComponent.tsx
@@ -0,0 +1,111 @@
+import SingleSlideImages from "../../components/singleProduct/SingleSlideImages";
+import Images from "../../components/imageComponent/Images";
+import { RiArrowDownSFill } from "react-icons/ri";
+import SelectSize from "../../components/singleProduct/SelectSizeComponent";
+import Button from "../../components/button/Button";
+import { CiHeart } from "react-icons/ci";
+import { ResponseIProducts } from "../../types/typeDatas";
+
+type SinglePageproducts = {
+  data: ResponseIProducts;
+  isOn: boolean;
+  setIsOn: React.Dispatch<React.SetStateAction<boolean>>;
+  chosenSize: string;
+  setChoseSize: React.Dispatch<React.SetStateAction<string>>;
+  handleSelectVisibility: () => void;
+  handleAddToCart: () => void;
+  customClass?: string;
+};
+
+const SinglepageComponent = ({
+  data,
+  isOn,
+  setIsOn,
+  chosenSize,
+  setChoseSize,
+  handleSelectVisibility,
+  handleAddToCart,
+  customClass,
+}: SinglePageproducts) => {
+  return (
+    <section className={`${customClass}-container`}>
+      <div className={`${customClass}-single-content`}>
+        <section className={`${customClass}-images-container`}>
+          <div className={`${customClass}-slide`}>
+            <SingleSlideImages {...data} />
+          </div>
+          <div className={`${customClass}-img`}>
+            <Images
+              src={data?.data.product.image[0]}
+              alt={data?.data.product.tailorname}
+              title={data?.data.product.tailorname}
+            />
+          </div>
+        </section>
+        <article className={`${customClass}-article`}>
+          <h3 className={`${customClass}-heading-3`}>
+            {data?.data.product.tailorname}
+          </h3>
+          <div className={`${customClass}-pricing`}>
+            <h4 className={`${customClass}-heading-4`}>
+              {data?.data.product.price}€
+            </h4>
+            <p>Includes VTA</p>
+          </div>
+          <p className={`${customClass}-p`}>
+            <span className={`${customClass}-color`}>Color</span>:{" "}
+            {data?.data.product.color}
+          </p>
+          {/*//TODO: create `UniqueColor` component*/}
+          <div className={`${customClass}-slide-article`}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className={`${customClass}-size-container`}>
+            <div
+              className={`${customClass}-size-title`}
+              onClick={handleSelectVisibility}
+            >
+              <p>{chosenSize === "" ? "Select your size" : chosenSize}</p>
+              <RiArrowDownSFill />
+            </div>
+            <div className={`${customClass}-size-content`}>
+              <div
+                className={
+                  isOn
+                    ? `${customClass}-content-content isVisible`
+                    : `${customClass}-content-content`
+                }
+              >
+                {/*//TODO:after I modified the backend, make sure to use the _id for the key!!!*/}
+                {data?.data.product.size?.map((onesize) => (
+                  <SelectSize
+                    onesize={onesize}
+                    chosenSize={chosenSize}
+                    setChoseSize={setChoseSize}
+                    setIsOn={setIsOn}
+                    key={onesize}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className={`${customClass}-add-like-btns`}>
+            {/*if there is no size don't add to cart eh!!!!*/}
+            <Button
+              as="button"
+              className={`${customClass}-add-btn`}
+              handleClick={handleAddToCart}
+            >
+              Add to Cart
+            </Button>
+            <CiHeart size="2.5rem" />
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default SinglepageComponent;
--- frontend/src/pages/Singleproduct.page.tsx
+++ frontend/src/pages/Singleproduct.page.tsx
@@ -6,12 +6,7 @@ import { useState } from "react";
 import useCart from "../hooks/useCart";
 import DetailComponent from "../components/singleProduct/DetailComponent";
 import { BsArrowLeft } from "react-icons/bs";
-import SingleSlideImages from "../components/singleProduct/SingleSlideImages";
-import Images from "../components/imageComponent/Images";
-import { RiArrowDownSFill } from "react-icons/ri";
-import SelectSize from "../components/singleProduct/SelectSizeComponent";
-import Button from "../components/button/Button";
-import { CiHeart } from "react-icons/ci";
+import SinglepageComponent from "../components/singleProduct/SinglepageComponent";
 
 const SingleproductPage = () => {
   const [isOn, setIsOn] = useState(false);
@@ -23,6 +18,7 @@ const SingleproductPage = () => {
   const { data, status, isLoading, isError, error } =
     useFetchSingleProduct(typedId);
   const product = data?.data.product;
+  const defaulClassName = "single";
 
   if (isLoading) return <h2>Loading...</h2>;
   if (isError) return <p>{error.message}</p>;
@@ -49,87 +45,16 @@ const SingleproductPage = () => {
               </div>
               {/*//TODO: Add an other reusable component for detail in cardspage*/}
               {status === "success" ? (
-                <section className="single-container">
-                  <div className="single-single-content">
-                    <section className="single-images-container">
-                      <div className="single-slide">
-                        <SingleSlideImages {...data} />
-                      </div>
-                      <div className="single-img">
-                        <Images
-                          src={data?.data.product.image[0]}
-                          alt={data?.data.product.tailorname}
-                          title={data?.data.product.tailorname}
-                        />
-                      </div>
-                    </section>
-                    <article className="single-article">
-                      <h3 className="single-heading-3">
-                        {data?.data.product.tailorname}
-                      </h3>
-                      <div className="single-pricing">
-                        <h4 className="single-heading-4">
-                          {data?.data.product.price}€
-                        </h4>
-                        <p>Includes VTA</p>
-                      </div>
-                      <p className="single-p">
-                        <span className="single-color">Color</span>:{" "}
-                        {data?.data.product.color}
-                      </p>
-                      {/*//TODO: create `UniqueColor` component*/}
-                      <div className="single-slide-article">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                      </div>
-                      <div className="single-size-container">
-                        <div
-                          className="single-size-title"
-                          onClick={handleSelectVisibility}
-                        >
-                          <p>
-                            {chosenSize === ""
-                              ? "Select your size"
-                              : chosenSize}
-                          </p>
-                          <RiArrowDownSFill />
-                        </div>
-                        <div className="single-size-content">
-                          <div
-                            className={
-                              isOn
-                                ? "single-content-content isVisible"
-                                : "single-content-content"
-                            }
-                          >
-                            {/*//TODO:after I modified the backend, make sure to use the _id for the key!!!*/}
-                            {data?.data.product.size?.map((onesize) => (
-                              <SelectSize
-                                onesize={onesize}
-                                chosenSize={chosenSize}
-                                setChoseSize={setChoseSize}
-                                setIsOn={setIsOn}
-                                key={onesize}
-                              />
-                            ))}
-                          </div>
-                        </div>
-                      </div>
-                      <div className="single-add-like-btns">
-                        {/*if there is no size don't add to cart eh!!!!*/}
-                        <Button
-                          as="button"
-                          className="single-add-btn"
-                          handleClick={handleAddToCart}
-                        >
-                          Add to Cart
-                        </Button>
-                        <CiHeart size="2.5rem" />
-                      </div>
-                    </article>
-                  </div>
-                </section>
+                <SinglepageComponent
+                  data={data}
+                  handleSelectVisibility={handleSelectVisibility}
+                  handleAddToCart={handleAddToCart}
+                  isOn={isOn}
+                  setIsOn={setIsOn}
+                  chosenSize={chosenSize}
+                  setChoseSize={setChoseSize}
+                  customClass={defaulClassName}
+                />
               ) : (
                 <>Fallback component with a message!</>
               )}
