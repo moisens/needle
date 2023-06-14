19acd26564e65a31611a178c151e47c4beee228b
moise <mose.ns@hotmail.com>
2023-06-14 11:20:33 +0200

    LikeContext add like functionality

--- frontend/src/context/LikeContext.tsx
+++ frontend/src/context/LikeContext.tsx
@@ -1,6 +1,5 @@
 import { createContext, ReactNode } from "react";
 
-
 export type IProducts = {
   readonly _id: string;
   tailorname: string;
@@ -19,25 +18,86 @@ export type IProducts = {
   qty: number;
 };
 
-export type ChildrenLikeType = {
-  children: ReactNode
+export interface ChildrenLikeProps {
+  children: ReactNode;
 }
 
-
-
-
-
-const Likecontext = createContext({})
-
-
-export const LikeContextProvider = ({ children }: ChildrenLikeType) => {
-
-  return <Likecontext.Provider value={{}}>
-    { children }
-  </Likecontext.Provider>
+export interface ILikeState {
+  like: IProducts[];
 }
 
+export interface ReducerAction {
+  type: string;
+  payload?: IProducts;
+}
 
+export const REDUCERS_ACTIONS_TYPE = {
+  ADD: "ADD",
+  REMOVE: "REMOVE",
+};
 
+export const reducer = (
+  state: ILikeState,
+  action: ReducerAction
+): ILikeState => {
+  if (action.type === REDUCERS_ACTIONS_TYPE.ADD) {
+    if (!action.payload)
+      throw new Error("action.payload is missing in ADD functionality");
 
+    const {
+      _id,
+      tailorname,
+      color,
+      price,
+      description,
+      clothesmaterials,
+      image,
+      maincategories,
+      subcategories,
+      size,
+      featured,
+      inventory,
+      averageRating,
+      numOfReviews,
+    } = action.payload;
 
+    const filtredItems: IProducts[] = state.like.filter(
+      (item) => item._id !== _id
+    );
+    const itemsExist: IProducts | undefined = state.like.find(
+      (item) => item._id === _id
+    );
+    const qty = itemsExist ? itemsExist.qty + 1 : 1;
+
+    return {
+      ...state,
+      like: [
+        ...filtredItems,
+        {
+          _id,
+          tailorname,
+          color,
+          price,
+          description,
+          clothesmaterials,
+          image,
+          maincategories,
+          subcategories,
+          size,
+          featured,
+          inventory,
+          averageRating,
+          numOfReviews,
+          qty,
+        },
+      ],
+    };
+  }
+  throw new Error("Unidentified reducer action type!");
+};
+
+const Likecontext = createContext({});
+
+export const LikeContextProvider = ({ children }: ChildrenLikeProps) => {
+  return <Likecontext.Provider value={{}}>{children}</Likecontext.Provider>;
+};
